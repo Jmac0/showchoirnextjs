9db5c66e87d37e5a9e2f03f478c3fa04b28c4177
JamieMac <Jamiejmackenzie@gmail.com>
2023-05-22 12:03:27 +0100

    Test Home component

---
+++ __tests__/ui/index.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../../src/pages";
+
+describe("index.tsx", () => {
+  it("should render the correct heading", () => {
+    render(<Home />);
+
+    expect(
+      screen.getByRole("heading", { name: /welcome to show choir/i })
+    ).toBeInTheDocument();
+  });
+});
