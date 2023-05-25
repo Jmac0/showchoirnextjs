1d9791dc9feb8df48f8b580b8635bdd34800f4ef
JamieMac <Jamiejmackenzie@gmail.com>
2023-05-25 14:11:45 +0100

    Change heading in index to DEVELOPMENT!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>Show Choir DEVELOPMENT BRANCH</h1>
+        <h1>Show Choir DEVELOPMENT BRANCH!</h1>
       </main>
 
       <footer>Footer</footer>
