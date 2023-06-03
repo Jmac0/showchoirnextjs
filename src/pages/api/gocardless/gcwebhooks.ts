2da5b0afe2121c3c27a43ef55cf4f921c466eb4e
JamieMac <Jamiejmackenzie@gmail.com>
2023-06-03 20:18:35 +0100

    Refactor gcwebhooks

--- src/pages/api/gocardless/gcwebhooks.ts
+++ src/pages/api/gocardless/gcwebhooks.ts
@@ -1,11 +1,11 @@
+/* eslint-disable camelcase */
 /* eslint-disable  @typescript-eslint/no-var-requires  */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from "axios";
+import { format } from "date-fns";
 import { buffer } from "micro";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import dbConnect from "@/src/lib/dbConnect";
-import { getCustomerFromGoCardless } from "@/src/lib/helpers/getCutomerGoCardless";
 import type { GocardlessWebhookEvent } from "@/src/types/types";
 
 const Members = require("../../../lib/models/member");
@@ -18,7 +18,6 @@ const client = gocardless(
   constants.Environments.Sandbox
 );
 const webhookEndpointSecret = process.env.GC_WEBHOOK_SECRET;
-const baseUrl = process.env.BASE_URL as string | undefined;
 /* 🛑 REMEMBER TO START NGROK FOR LOCAL TESTING */
 // Function with switch block to handle incoming events from Gocardless
 // const processEvents = async (event: GocardlessWebhookEvent) => {
@@ -62,6 +61,32 @@ const baseUrl = process.env.BASE_URL as string | undefined;
 //   }
 //   return null;
 // };
+//
+
+const addGocardlessRecordsToCustomer = async (gocardlessCustomerLinks: {
+  customer: string;
+  mandate_request_mandate: string;
+}) => {
+  const { customer, mandate_request_mandate } = gocardlessCustomerLinks;
+  await dbConnect();
+  const currentDate = format(new Date(), "dd/MM/yyyy");
+  // check that the customer property is present in the request body
+  if (customer) {
+    // Get the customer info details from GoCardles
+    const newCustomer = await client.customers.find(customer);
+    // Up date customer in DB
+    await Members.findOneAndUpdate(
+      { email: newCustomer.email },
+      {
+        active_mandate: true,
+        mandate: mandate_request_mandate,
+        go_cardless_id: customer,
+        direct_debit_started: currentDate,
+      },
+      { new: true }
+    );
+  }
+};
 
 // Handle the coming Webhook and check its signature.
 const parseEvents = (
@@ -91,17 +116,14 @@ export default async function handler(
   // get signature from headers
   const signature = req.headers["webhook-signature"]?.toString();
   // check signature and if ok return array of events
-  const checkSignature = parseEvents(body, signature);
+  const eventsArray = parseEvents(body, signature);
   // if array pass to event to appropriate handler
-  if (checkSignature) {
-    checkSignature
-      .forEach(async (event: GocardlessWebhookEvent) => {
-        if (event.action === "fulfilled") console.log("CALLED webhooks", event);
-        axios.post(`${baseUrl}/api/gocardless/tempgetcustomer`, {
-          gocardlessCustomerLinks: event.links,
-        }).catch((err) => {throw new Error(err.message)})
-        return null;
-      })
+  if (eventsArray) {
+    eventsArray.forEach(async (event: GocardlessWebhookEvent) => {
+      if (event.action === "fulfilled" && event.links.customer) {
+        await addGocardlessRecordsToCustomer(event.links);
+      }
+    });
   }
 
   res.status(200).json("ok");
