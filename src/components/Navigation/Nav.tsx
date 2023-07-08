1906a094cfe9501d9993e831597d6b5ee0c1508c
JamieMac <Jamiejmackenzie@gmail.com>
2023-07-08 22:04:14 +0100

    Code and test Hero component, small changes to hamburger

--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -41,7 +41,7 @@ export function Nav({ pathData = [] }: Props) {
       <Link
         key={item.slug}
         href={item.slug}
-        className={`${item.displayText.toLowerCase()}-mobile mb-10 px-3 font-heading text-xl text-white hover:text-gray-600 md:mb-0`}
+        className={`${item.displayText.toLowerCase()}-mobile mb-10 px-3 font-heading text-3xl text-lightBlack hover:text-gray-600 md:mb-0`}
       >
         {item.displayText}
       </Link>
@@ -54,34 +54,39 @@ export function Nav({ pathData = [] }: Props) {
       <button
         data-testid="hamburger-icon"
         type="button"
-        className="z-40 m-4 flex h-6 w-6 flex-col items-center justify-between md:hidden"
         onClick={handleClick}
+        className={`z-40 m-4 flex h-12 w-12 flex-col content-center items-center justify-between
+        md:hidden
+        `}
       >
         <div
-          className={`mb-1 h-1 w-6 origin-left rounded-full bg-gray-50 ${
-            open ? "ml-1 mt-1 w-[22px] rotate-45" : "rotate-0"
-          } transition-all`}
+          className={`mb-1 mt-3 h-1 w-8 origin-left rounded-full bg-gray-50 ${
+            open ? "top-0 w-2/3 rotate-45  bg-lightBlack" : "rotate-0"
+          } transition-all duration-300
+          `}
         />
         <div
-          className={`h-1 w-6 origin-left rounded-full bg-gray-50 ${
+          className={`h-1 w-8 origin-left rounded-full bg-gray-50 ${
             open && "hidden"
-          } transition-all`}
+          } transition-all duration-300
+          `}
         />
         <div
-          className={`mt-1 h-1 w-6 origin-left rounded-full bg-gray-50 ${
-            open ? "ml-1 w-[22px] -rotate-45" : "rotate-0"
-          } transition-all`}
+          className={`mb-3 mt-1 h-1 w-8 origin-left rounded-full bg-gray-50 ${
+            open ? "mb-[9px] w-2/3 -rotate-45 bg-lightBlack" : "rotate-0"
+          } transition-all duration-300
+          `}
         />
       </button>
       {/* draw */}
       <nav
-        className={`absolute ${
+        className={`absolute z-20 ${
           open ? "left-0" : "-left-full"
-        } z-10 flex h-full w-2/4 flex-col bg-gold bg-gradient-to-b from-lightGold to-gold pl-16 pt-28 transition-all duration-300 ease-in-out md:hidden`}
+        } z-10 flex h-full w-2/4 flex-col bg-gold bg-gradient-to-b from-amber-300 to-gold pl-16 pt-28 transition-all duration-300 ease-in-out md:hidden`}
       >
         <Link
           href="/"
-          className="home-mobile mb-10 px-3 font-heading text-xl text-white hover:text-gray-600 "
+          className="home-mobile mb-10 px-3 font-heading text-3xl text-lightBlack hover:text-gray-600 "
         >
           Home
         </Link>
@@ -93,15 +98,15 @@ export function Nav({ pathData = [] }: Props) {
         type="button"
         aria-label="button"
         onClick={handleClick}
-        className={`hamburger-overlay fixed z-[1] h-screen w-screen  bg-gray-700  md:hidden ${
-          open ? "opacity-90" : " invisible opacity-0"
+        className={`hamburger-overlay fixed z-[1] h-screen w-screen  bg-black  md:hidden ${
+          open ? "opacity-70" : " invisible opacity-0"
         } transition-all duration-500 `}
       />
 
       {/* desktop nav container */}
       <nav
         data-testid="desktop-nav"
-        className="absolute hidden h-16 w-full flex-row items-center justify-start bg-transparent pl-16 md:flex md:h-28  md:justify-start"
+        className="absolute hidden h-16 w-full flex-row items-center justify-start bg-transparent pl-16 md:z-30 md:flex md:h-28  md:justify-start"
       >
         <Link
           href="/"
