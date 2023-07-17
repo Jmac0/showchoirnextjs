47603e547c6dd616b0400d01965c1bdd1470e765
JamieMac <Jamiejmackenzie@gmail.com>
2023-07-17 13:43:41 +0100

    Code membership options info component
    Style link in MembershipOptionsInfo

---
+++ src/components/MembershipOptionsContainer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { MembershipOptionInfo } from "./MembershipOptionsInfo";
+
+type Props = {
+  flexiInfo: string;
+  monthlyInfo: string;
+};
+function MembershipOptionsContainer({ flexiInfo, monthlyInfo }: Props) {
+  return (
+    <div className="flex flex-col flex-wrap items-center justify-center md:flex-row">
+      <MembershipOptionInfo markdown={monthlyInfo} navigateTo="monthly" />
+      <MembershipOptionInfo markdown={flexiInfo} navigateTo="*" />
+    </div>
+  );
+}
+
+export default MembershipOptionsContainer;
