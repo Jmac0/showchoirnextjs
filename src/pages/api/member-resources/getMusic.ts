524c70e2af47b6f4e83c62c71d31abed075e335e
JamieMac <Jamiejmackenzie@gmail.com>
2024-02-24 13:15:07 +0000

    Refactor get objects into fetchS3Objects function

--- src/pages/api/member-resources/getMusic.ts
+++ src/pages/api/member-resources/getMusic.ts
@@ -1,25 +1,12 @@
-import {
-  GetObjectCommand,
-  ListObjectsV2Command,
-  S3Client,
-} from "@aws-sdk/client-s3";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
 
+import { fetchS3Objects, s3 } from "@/src/lib/aws/fetchS3Objects";
 import { authOptions } from "@/src/pages/api/auth/[...nextauth]";
 
 const bucketName = process.env.S3_BUCKET_NAME as string;
-const bucketRegion = process.env.S3_BUCKET_REGION as string;
-const s3AuthOptions = {
-  region: bucketRegion,
-  credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY as string,
-    secretAccessKey: process.env.S3_SECRET_KEY as string,
-  },
-};
-const s3 = new S3Client(s3AuthOptions);
-
 export default async function getMusic(
   req: NextApiRequest,
   res: NextApiResponse
@@ -27,57 +14,36 @@ export default async function getMusic(
   const session = await getServerSession(req, res, authOptions);
   if (!session) {
     return res.status(401).json({ failure: "unauthenticated" });
+    // array of paths to all objects in /Harmonies in S3 bucket
   }
 
-  const listObjectsCommand = new ListObjectsV2Command({
-    Bucket: bucketName,
-    StartAfter: "Harmonies/",
-
-    // The default and maximum number of keys returned is 1000. This limits it to
-    // one for dlemonstration purposes.
-    MaxKeys: 500,
-  });
-  // array of paths to all objects in /Harmonies in S3 bucket
-  let pathArray: any[] = [];
-  // array of objects, the path and a signed url
-  const harmonyObjectArray: object[] = [];
   try {
-    let isTruncated: boolean | undefined = true;
+    // array of objects, the path and a signed url
+    const harmonyObjectArray: object[] = [];
 
-    while (isTruncated) {
-      // eslint-disable-next-line no-await-in-loop
-      const { Contents, IsTruncated, NextContinuationToken } = await s3.send(
-        listObjectsCommand
-      );
-      if (Contents) {
-        pathArray = Contents.map((c) => c.Key);
-        isTruncated = IsTruncated;
-        listObjectsCommand.input.ContinuationToken = NextContinuationToken;
-      }
-    }
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
+    const s3ObjectPaths = await fetchS3Objects("Harmonies/");
+
+    // get presigned url frm aws, and return an object with the url and file path
+    const createPresignedUrlWithClient = async (pathToFile: string) => {
+      const command = new GetObjectCommand({
+        Bucket: bucketName,
+        Key: pathToFile,
+        ResponseContentDisposition: "attachment",
+      });
+
+      const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+      const obj = { url, name: pathToFile };
+      harmonyObjectArray.push(obj);
+    };
+
+    await Promise.all(
+      s3ObjectPaths.map(async (path: string) => {
+        await createPresignedUrlWithClient(path);
+      })
+    );
+    return res.status(200).json({ url: harmonyObjectArray });
+  } catch (err: any) {
+    return res.status(500).json({ error: err.response.message });
   }
-  // get presigned url frm aws, and return an object with the url and file path
-  const createPresignedUrlWithClient = async (pathToFile: string) => {
-    const command = new GetObjectCommand({
-      Bucket: bucketName,
-      Key: pathToFile,
-      ResponseContentDisposition: "attachment",
-    });
-
-    const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-
-    const obj = { url, name: pathToFile };
-    harmonyObjectArray.push(obj);
-  };
-  pathArray = pathArray.slice(1);
-
-  await Promise.all(
-    pathArray.map(async (path: string) => {
-      await createPresignedUrlWithClient(path);
-    })
-  );
-  return res.status(200).json({ url: harmonyObjectArray });
 }
